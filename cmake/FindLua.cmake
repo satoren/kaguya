2086ed348688f9eb3f67d78b088cd8caa4e0a295
satoren <satoreyo@hotmail.com>
2015-12-19 20:23:14 +0900

    use lua-5.3.2

--- cmake/FindLua.cmake
+++ cmake/FindLua.cmake
@@ -1,5 +1,17 @@
 
 if(NOT LUA_INCLUDE_DIRS)
+  #search local directory
+  set(LOCAL_LUA_DIRECTORY lua-5.3.2)
+  if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_LUA_DIRECTORY})
+    file(COPY Lua-CMakeLists.txt DESTINATION ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_LUA_DIRECTORY})
+    file(RENAME ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_LUA_DIRECTORY}/Lua-CMakeLists.txt ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_LUA_DIRECTORY}/CMakeLists.txt)
+    add_subdirectory(${LOCAL_LUA_DIRECTORY})
+   set(LUA_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_LUA_DIRECTORY}/src)
+   set(LUA_LIBRARIES liblua)
+  endif()
+endif()
+
+if(NOT LUA_INCLUDE_DIRS)
   find_package(PkgConfig)
   set(LUA_SEARCHVERS lua5.3 lua5.2 luajit lua5.1)
   foreach(modulename ${LUA_SEARCHVERS})
@@ -7,20 +19,10 @@ if(NOT LUA_INCLUDE_DIRS)
   endforeach(modulename)
 endif(NOT LUA_INCLUDE_DIRS)
 
-
 if(NOT LUA_INCLUDE_DIRS)
-  #search local directory
-  set(LOCAL_LUA_DIRECTORY lua-5.3.1)
-  if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_LUA_DIRECTORY})
-    file(COPY Lua-CMakeLists.txt DESTINATION ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_LUA_DIRECTORY})
-    file(RENAME ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_LUA_DIRECTORY}/Lua-CMakeLists.txt ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_LUA_DIRECTORY}/CMakeLists.txt)
-    add_subdirectory(${LOCAL_LUA_DIRECTORY})
-   set(LUA_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_LUA_DIRECTORY}/src)
-   set(LUA_LIBRARIES liblua)
-  else()
-    message(SEND_ERROR "Can't find lua library")   
-  endif()
-endif()
+ message(SEND_ERROR "Can't find lua library")
+endif(NOT LUA_INCLUDE_DIRS)
+
 
 if(NOT EXISTS ${LUA_INCLUDE_DIRS}/lua.h)
   message(SEND_ERROR "Can't find lua.h in ${LUA_INCLUDE_DIRS}")
