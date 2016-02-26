8072f8721975f606a457aee18927e31fd0d0f778
satoren <satoreyo@hotmail.com>
2016-02-26 17:30:56 +0900

    add script for generate of one header file

---
+++ utils/generate_one_header.py
@@ -0,0 +1,26 @@
+
+import os
+import sys
+import re
+
+def inner_include_path(line):
+    m = re.search('#include\s\"(?P<headername>.*)\"',line)
+    if m:
+        return m.group('headername')
+    return None;
+    
+def parseheader(out,filepath,basedir,onceincludedfiles):
+    
+    for line in open(os.path.join(basedir,filepath), 'r'):
+        if line.find('#pragma once') != -1:
+            onceincludedfiles.append(os.path.join(basedir,filepath))
+        path = inner_include_path(line)
+        if path:
+            if not os.path.join(basedir,path) in onceincludedfiles:
+                parseheader(out,path,basedir,onceincludedfiles)
+        else:
+            out.write(line)
+
+if __name__ == "__main__":
+    onceincludedfiles=[]
+    parseheader(sys.stdout,'kaguya\\kaguya.hpp','..\\include\\',onceincludedfiles)
