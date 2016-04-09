5d76080122c2b0688dad1bbff49cc80a45c6b4b8
satoren <satoreyo@hotmail.com>
2016-04-10 07:47:36 +0900

    compile error at MSVC2013

--- test/test_util.hpp
+++ test/test_util.hpp
@@ -8,8 +8,12 @@ namespace kaguya_test_util
 {
 	inline std::string to_string(int v)
 	{
-		char buffer[64] = { 0 };
-		snprintf(buffer,64, "%d", v);
+		char buffer[64] = {};
+#if (defined(_MSC_VER) && _MSC_VER < 1900)
+		sprintf_s(buffer, "%d", v);
+#else
+		snprintf(buffer, 64, "%d", v);
+#endif
 		return buffer;
 	}
 
