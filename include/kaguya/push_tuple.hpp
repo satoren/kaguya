44c9154f77b3640c1f65afb13619e09a372c3a69
satoren <satoreyo@hotmail.com>
2016-06-15 19:01:21 +0900

    refactoring push and get for tuple

--- include/kaguya/push_tuple.hpp
+++ include/kaguya/push_tuple.hpp
@@ -10,139 +10,47 @@
 
 namespace kaguya
 {
-	template<>
-	struct lua_type_traits<standard::tuple<> >
+#if KAGUYA_USE_CPP11
+	namespace detail
 	{
-		static int push(lua_State* l, const standard::tuple<>& v)
+		template<std::size_t... indexes>struct index_tuple {};
+		template<std::size_t first, std::size_t last, class result = index_tuple<>, bool flag = first >= last>
+		struct index_range
 		{
-			int count = 0;
-			return count;
+			using type = result;
+		};
+		template<std::size_t step, std::size_t last, std::size_t... indexes>
+		struct index_range<step, last, index_tuple<indexes...>, false>
+			: index_range<step + 1, last, index_tuple<indexes..., step>>
+		{};
+
+		template<std::size_t... Indexes,class... Args>
+		int push_tuple(lua_State* l, index_tuple<Indexes...> index, std::tuple<Args...>&& v)
+		{
+			return util::push_args(l, std::get<Indexes>(v)...);
+		}
+	}
+	template<class... Args>
+	struct lua_type_traits<standard::tuple<Args...> >
+	{
+		static int push(lua_State* l, std::tuple<Args...>&& v)
+		{
+			typename detail::index_range<0, sizeof...(Args)>::type index;
+			return detail::push_tuple(l, index, std::forward< std::tuple<Args...> >(v));
 		}
 	};
-	template<typename T1>
-	struct lua_type_traits<standard::tuple<T1> >
-	{
-		static int push(lua_State* l, const standard::tuple<T1>& v)
-		{
-			int count = 0;
-			count += lua_type_traits<T1>::push(l, standard::get<0>(v));
-			return count;
-		}
+#else
+#define KAGUYA_PP_GET_DATA(N) standard::get<N-1>(v)
+#define KAGUYA_PUSH_TUPLE_DEF(N) template<KAGUYA_PP_TEMPLATE_DEF_REPEAT(N)>\
+	struct lua_type_traits<standard::tuple<KAGUYA_PP_TEMPLATE_ARG_REPEAT(N)> >\
+	{\
+		static int push(lua_State* l, const standard::tuple<KAGUYA_PP_TEMPLATE_ARG_REPEAT(N)>& v)\
+		{\
+			return util::push_args(l, KAGUYA_PP_REPEAT_ARG(N, KAGUYA_PP_GET_DATA));\
+		}\
 	};
-	template<typename T1, typename T2>
-	struct lua_type_traits<standard::tuple<T1, T2> >
-	{
-		static int push(lua_State* l, const standard::tuple<T1, T2>& v)
-		{
-			int count = 0;
-			count += lua_type_traits<T1>::push(l, standard::get<0>(v));
-			count += lua_type_traits<T2>::push(l, standard::get<1>(v));
-			return count;
-		}
-	};
-	template<typename T1, typename T2, typename T3>
-	struct lua_type_traits<standard::tuple<T1, T2, T3> >
-	{
-		static int push(lua_State* l, const standard::tuple<T1, T2, T3>& v)
-		{
-			int count = 0;
-			count += lua_type_traits<T1>::push(l, standard::get<0>(v));
-			count += lua_type_traits<T2>::push(l, standard::get<1>(v));
-			count += lua_type_traits<T3>::push(l, standard::get<2>(v));
-			return count;
-		}
-	};
-	template<typename T1, typename T2, typename T3, typename T4>
-	struct lua_type_traits<standard::tuple<T1, T2, T3, T4> >
-	{
-		static int push(lua_State* l, const standard::tuple<T1, T2, T3, T4>& v)
-		{
-			int count = 0;
-			count += lua_type_traits<T1>::push(l, standard::get<0>(v));
-			count += lua_type_traits<T2>::push(l, standard::get<1>(v));
-			count += lua_type_traits<T3>::push(l, standard::get<2>(v));
-			count += lua_type_traits<T4>::push(l, standard::get<3>(v));
-			return count;
-		}
-	};
-	template<typename T1, typename T2, typename T3, typename T4, typename T5>
-	struct lua_type_traits<standard::tuple<T1, T2, T3, T4, T5> >
-	{
-		static int push(lua_State* l, const standard::tuple<T1, T2, T3, T4, T5>& v)
-		{
-			int count = 0;
-			count += lua_type_traits<T1>::push(l, standard::get<0>(v));
-			count += lua_type_traits<T2>::push(l, standard::get<1>(v));
-			count += lua_type_traits<T3>::push(l, standard::get<2>(v));
-			count += lua_type_traits<T4>::push(l, standard::get<3>(v));
-			count += lua_type_traits<T5>::push(l, standard::get<4>(v));
-			return count;
-		}
-	};
-	template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-	struct lua_type_traits<standard::tuple<T1, T2, T3, T4, T5, T6> >
-	{
-		static int push(lua_State* l, const standard::tuple<T1, T2, T3, T4, T5, T6>& v)
-		{
-			int count = 0;
-			count += lua_type_traits<T1>::push(l, standard::get<0>(v));
-			count += lua_type_traits<T2>::push(l, standard::get<1>(v));
-			count += lua_type_traits<T3>::push(l, standard::get<2>(v));
-			count += lua_type_traits<T4>::push(l, standard::get<3>(v));
-			count += lua_type_traits<T5>::push(l, standard::get<4>(v));
-			count += lua_type_traits<T6>::push(l, standard::get<5>(v));
-			return count;
-		}
-	};
-	template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
-	struct lua_type_traits<standard::tuple<T1, T2, T3, T4, T5, T6, T7> >
-	{
-		static int push(lua_State* l, const standard::tuple<T1, T2, T3, T4, T5, T6, T7>& v)
-		{
-			int count = 0;
-			count += lua_type_traits<T1>::push(l, standard::get<0>(v));
-			count += lua_type_traits<T2>::push(l, standard::get<1>(v));
-			count += lua_type_traits<T3>::push(l, standard::get<2>(v));
-			count += lua_type_traits<T4>::push(l, standard::get<3>(v));
-			count += lua_type_traits<T5>::push(l, standard::get<4>(v));
-			count += lua_type_traits<T6>::push(l, standard::get<5>(v));
-			count += lua_type_traits<T7>::push(l, standard::get<6>(v));
-			return count;
-		}
-	};
-	template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8>
-	struct lua_type_traits<standard::tuple<T1, T2, T3, T4, T5, T6, T7, T8> >
-	{
-		static int push(lua_State* l, const standard::tuple<T1, T2, T3, T4, T5, T6, T7, T8>& v)
-		{
-			int count = 0;
-			count += lua_type_traits<T1>::push(l, standard::get<0>(v));
-			count += lua_type_traits<T2>::push(l, standard::get<1>(v));
-			count += lua_type_traits<T3>::push(l, standard::get<2>(v));
-			count += lua_type_traits<T4>::push(l, standard::get<3>(v));
-			count += lua_type_traits<T5>::push(l, standard::get<4>(v));
-			count += lua_type_traits<T6>::push(l, standard::get<5>(v));
-			count += lua_type_traits<T7>::push(l, standard::get<6>(v));
-			count += lua_type_traits<T8>::push(l, standard::get<7>(v));
-			return count;
-		}
-	};
-	template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9>
-	struct lua_type_traits<standard::tuple<T1, T2, T3, T4, T5, T6, T7, T8, T9> >
-	{
-		static int push(lua_State* l, const standard::tuple<T1, T2, T3, T4, T5, T6, T7, T8, T9>& v)
-		{
-			int count = 0;
-			count += lua_type_traits<T1>::push(l, standard::get<0>(v));
-			count += lua_type_traits<T2>::push(l, standard::get<1>(v));
-			count += lua_type_traits<T3>::push(l, standard::get<2>(v));
-			count += lua_type_traits<T4>::push(l, standard::get<3>(v));
-			count += lua_type_traits<T5>::push(l, standard::get<4>(v));
-			count += lua_type_traits<T6>::push(l, standard::get<5>(v));
-			count += lua_type_traits<T7>::push(l, standard::get<6>(v));
-			count += lua_type_traits<T8>::push(l, standard::get<7>(v));
-			count += lua_type_traits<T9>::push(l, standard::get<8>(v));
-			return count;
-		}
-	};
+	KAGUYA_PP_REPEAT_DEF(9, KAGUYA_PUSH_TUPLE_DEF)
+#undef KAGUYA_PP_GET_DATA
+#undef KAGUYA_PUSH_TUPLE_DEF
+#endif
 }
