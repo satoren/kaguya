406d016f7e97db3d2b52fac0050d3571ce916b52
satoren <satoreyo@hotmail.com>
2016-06-16 02:12:36 +0900

    add test_set= parameter to command line argument

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,7 +1,29 @@
+#include<vector>
+#include<string>
+#include<sstream>
+
 #include "test_util.hpp"
 
-int main()
+int main(int argc,const char* argv[])
 {
+	std::vector<std::string> test_set;
+
+	const std::string run_test_opt = "--run_test=";
+	for (int i = 1; i < argc; ++i)
+	{
+		if (strncmp(argv[i], run_test_opt.c_str(), run_test_opt.size()) == 0)
+		{
+			std::stringstream opt(argv[i] + run_test_opt.size());
+			while (opt.good())
+			{
+				std::string testname;
+				getline(opt, testname, ',');
+				test_set.push_back(testname);
+			}
+		}
+	}
+	kaguya_test_util::TestRunner::instance().set_test_set(test_set);
+
 	bool test_result = kaguya_test_util::TestRunner::instance().execute();
 	return test_result ? 0 : -1;
 }
