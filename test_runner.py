4d7b8bdaa184b4d48bd7f7d26a04c75421cfa604
satoren <satoreyo@hotmail.com>
2016-07-21 02:39:47 +0900

    add more compiler version

--- test_runner.py
+++ test_runner.py
@@ -11,6 +11,7 @@ if not boost_dir:
     boost_dir='D:\\boost_1_60_0'
 
 lua_versions = ["lua-5.3.3","lua-5.2.4","lua-5.1.5"]
+
 test_msvc_vers = [("msvc2015","Visual Studio 14 2015","")
     ,("msvc2015win64","Visual Studio 14 2015 Win64","")
     ,("msvc2013","Visual Studio 12 2013","")
@@ -20,8 +21,20 @@ test_msvc_vers = [("msvc2015","Visual Studio 14 2015","")
     ,("msvc2008","Visual Studio 9 2008","-DADDITIONAL_INCLUDE_PATH="+boost_dir)]
 
 test_compilers = [
-    ('gcc','g++','-DCMAKE_CXX_FLAGS=-std=c++03')
-    ,('gcc','g++','-DCMAKE_CXX_FLAGS=-std=c++11')
+    ('gcc-3.3','g++-3.3','-DCMAKE_CXX_FLAGS=-std=c++03')
+    ('gcc-4.4','g++-4.4','-DCMAKE_CXX_FLAGS=-std=c++03')
+    ,('gcc-4.5','g++-4.5','-DCMAKE_CXX_FLAGS=-std=c++03')
+    ,('gcc-4.6','g++-4.6','-DCMAKE_CXX_FLAGS=-std=c++03')
+    ,('gcc-4.7','g++-4.7','-DCMAKE_CXX_FLAGS=-std=c++03')
+    ,('gcc-4.7','g++-4.7','-DCMAKE_CXX_FLAGS=-std=c++11')
+    ,('gcc-4.8','g++-4.8','-DCMAKE_CXX_FLAGS=-std=c++03')
+    ,('gcc-4.8','g++-4.8','-DCMAKE_CXX_FLAGS=-std=c++11')
+    ,('gcc-4.9','g++-4.9','-DCMAKE_CXX_FLAGS=-std=c++03')
+    ,('gcc-4.9','g++-4.9','-DCMAKE_CXX_FLAGS=-std=c++11')
+    ,('gcc-5','g++-5','-DCMAKE_CXX_FLAGS=-std=c++03')
+    ,('gcc-5','g++-5','-DCMAKE_CXX_FLAGS=-std=c++11')
+    ,('gcc-6','g++-6','-DCMAKE_CXX_FLAGS=-std=c++03')
+    ,('gcc-6','g++-6','-DCMAKE_CXX_FLAGS=-std=c++11')
     ,('clang','clang++','-DCMAKE_CXX_FLAGS=-std=c++03')
     ,('clang','clang++','-DCMAKE_CXX_FLAGS=-std=c++11')]
 
@@ -29,15 +42,15 @@ def build_and_exec_test(compiler,lua_version,build_type):
     ccompiler = compiler[0]
     cxxcompiler = compiler[1]
     addopt = compiler[2]
-    if os.system(cxxcompiler+' -v')!=0: return
-    
+    if os.system(cxxcompiler+' -v 2> /dev/null')!=0: return
+
     buildpath = "_build/"+compiler[0]+"_"+lua_version+"_"+build_type
     if not os.path.exists(buildpath):
         os.makedirs(buildpath)
     os.chdir(buildpath)
     ret = os.system('CC='+ccompiler+' CXX='+cxxcompiler+' cmake ../../ -DCMAKE_BUILD_TYPE=Debug '+addopt+' -DLUA_SEARCH_LIB_NAME='+lua_version+' -DCMAKE_BUILD_TYPE='+build_type)
     if ret != 0: raise Exception("cmake error at"+buildpath)
-    ret = os.system('make')
+    ret = os.system('make -j')
     if ret != 0: raise Exception("build error at"+buildpath)
     ret = os.system('ctest --output-on-failure')
     if ret != 0: raise Exception("test error at"+buildpath)
