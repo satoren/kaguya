2aad225bae8f00a6029e7b7e13f47e186ce63391
zmcj21 <96552328+zmcj21@users.noreply.github.com>
2022-05-06 12:48:14 +0300

    fix a stupid bug.

--- include/kaguya/metatable.hpp
+++ include/kaguya/metatable.hpp
@@ -1,4 +1,4 @@
-9// Copyright satoren
+// Copyright satoren
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
